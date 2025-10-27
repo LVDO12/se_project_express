d8b283e8677f2bc7255d182f877959afb6604268
LV <viplong12hh@gmail.com>
2025-10-27 14:17:53 -0500

    fix route calls

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const { createUser, loginUser } = require("../controllers/users");
-const {validateSignIn, validateSignUp} = require("../middlewares/validator");
+const {validateLogin, validateSignUp} = require("../middlewares/validator");
 
-router.post("/signin", validateSignIn, loginUser);
+router.post("/signin", validateLogin, loginUser);
 router.post("/signup", validateSignUp, createUser);
 router.use("/users", require("./users"));
 router.use("/items", require("./clothingItems"));
